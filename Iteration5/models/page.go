b1b7f847e66761b015dbb3e9eebaf8c40e7a393e
Serdar Kalaycı <serdarkalayci@Serdars-MacBook-Pro.local>
2018-05-01 14:53:10 +0300

    Start iteration 5

---
+++ Iteration5/models/page.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"gopkg.in/mgo.v2/bson"
+	"time"
+)
+
+// Page represents a web page to display its title and body
+type Page struct {
+	ID         bson.ObjectId `bson:"_id" json:"id"`
+	Title      string        `bson:"title" json:"title"`
+	Body       string        `bson:"body" json:"body"`
+	LastUpdate time.Time     `bson:"lastUpdate" json:"lastUpdate"`
+}
